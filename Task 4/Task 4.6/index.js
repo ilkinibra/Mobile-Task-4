c82a5527029566fd62e63fbf04560b573b2a8099
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2023-10-16 10:52:29 +0000

    Update index.js

--- Task 4/Task 4.6/index.js
+++ Task 4/Task 4.6/index.js
@@ -1,1 +1,14 @@
 //6. Obyektin hər hansısa bir sahəsinin olub olmadığını yoxluyan bir funksiya yazın.
+const person =
+{
+    Name: "İlkin",
+    Surname: "İbrahimov",
+}
+
+function yoxla(obj,key)
+{
+    return key in obj;
+}
+
+console.log(yoxla(person,"Name"));
+console.log(yoxla(person,"Age"));
